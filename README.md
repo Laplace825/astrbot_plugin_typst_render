3cb819fa3ecf80bde17549cd9948417b6c1d6453
Laplace825 <laplace.he@outlook.com>
2025-08-04 21:59:19 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Typst Render
+
+AstrBot 插件, 支持使用将信息中的 Typst 代码渲染为 png 发送.
+
+## 使用
+
+```
+\typ 
+"""
+
+"""
+```
+
+# 支持
+
+[帮助文档](https://astrbot.app)
---
+++ main.py
@@ -0,0 +1,37 @@
+import typst
+import tempfile
+import os
+
+from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.star import Context, Star, register
+from astrbot.api import logger
+
+
+@register("TypstRender", "Laplace", "渲染Typst代码并返回图片", "0.0.1")
+class TypstRender(Star):
+    def __init__(self, context: Context):
+        super().__init__(context)
+
+    async def initialize(self):
+        """实例化插件时自动获取temp目录, 目标文件将被存储在该目录。"""
+        self.temp_png_file_dir = tempfile.TemporaryDirectory()
+
+    # 注册指令的装饰器。指令名为 typ。注册成功后，发送 `/typ` 就会触发这个指令
+    @filter.command("typ")
+    async def typ(self, event: AstrMessageEvent):
+        message_str = event.message_str  # 用户发的纯文本消息字符串
+        if not message_str:
+            yield event.plain_result("请提供Typst代码进行渲染")
+        random_png_file_name = f"{event.get_session_id()}.png"
+        typst.compile(
+            message_str,
+            output=os.path.join(self.temp_png_file_dir.name, random_png_file_name),
+            format="png",
+        )
+        logger.info(f"Typst code rendered to {random_png_file_name} in temp directory.")
+        yield event.image_result(
+            os.path.join(self.temp_png_file_dir.name, random_png_file_name),
+        )
+
+    async def terminate(self):
+        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
